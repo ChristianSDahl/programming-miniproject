cc6df3a4e78ca81dc2be2369272cf01e81c8e855
Line Kjær Christensen <lkch16@student.aau.dk>
2017-11-02 11:14:24 +0100

    Props.h + Prop.cpp

---
+++ santa_game/santa_game/Props.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "Props.h"
+
+
+Props::Props()
+{
+}
+
+
+Props::~Props()
+{
+}
+
+float Props::fall() {
+
+}
+
+// Get's and setters
+float Props::get_x() {
+
+}
+
+void Props::set_x(float x) {
+
+}
+
+float Props::get_y() {
+
+}
+
+void Props::set_y(float y) {
+
+}
+
+float Props::get_gravity() {
+
+}
+
+void Props::set_gravity(float gravity) {
+
+}
+
+Mat Props::get_image() {
+
+}
+
+void Props::set_image(Mat image) {
+
+}
+
+int Props::get_type() {
+
+}
+
+void Props::set_type(int type) {
+
+}
---
+++ santa_game/santa_game/Props.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+class Props
+{
+	float x;
+	float y;
+	float gravity;
+	Mat img;
+	int type;
+
+public:
+	Props();
+	~Props();
+
+	float fall();
+
+	// Get's and Setters
+	float get_x();
+	void set_x(float x);
+	float get_y();
+	void set_y(float y);
+	float get_gravity();
+	void set_gravity(float gravity);
+	Mat get_image();
+	void set_image(Mat image);
+	int get_type();
+	void set_type(int type);
+
+};
+

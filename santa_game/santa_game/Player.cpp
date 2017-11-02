37dcb22c5a80127d0a68572046be656d8e71ef35
Unknown <nolse16@student.aau.dk>
2017-11-02 11:15:04 +0100

    Player.h and player.cpp

--- santa_game/santa_game/Player.cpp
+++ santa_game/santa_game/Player.cpp
@@ -10,3 +10,42 @@ Player::Player()
 Player::~Player()
 {
 }
+
+float Player::move(float x, float y) {
+
+}
+void Player::action() {
+
+}
+
+//Get's and Setter's
+float Player::get_x() {
+
+}
+void Player::set_x(float x) {
+
+}
+float Player::get_y() {
+
+}
+void Player::set_y(float y) {
+
+}
+float Player::get_score() {
+
+}
+void Player::set_score(float score) {
+
+}
+float Player::get_speed() {
+
+}
+void Player::set_speed(float speed) {
+
+}
+float Player::get_cooldown() {
+
+}
+void Player::set_cooldown(float cd) {
+
+}

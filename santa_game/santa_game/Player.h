12190431ec9b25652a511ed34927833daf2f4ba9
Unknown <nolse16@student.aau.dk>
2017-11-02 11:18:54 +0100

    fixed

--- santa_game/santa_game/Player.h
+++ santa_game/santa_game/Player.h
@@ -16,5 +16,19 @@ class Player
 public:
 	Player();
 	~Player();
+
+	float move(float x, float y);
+	void action();
+
+	float get_x();
+	void set_x(float x);
+	float get_y();
+	void set_y(float y);
+	float get_score();
+	void set_score(float score);
+	float get_speed();
+	void set_speed(float speed);
+	float get_cooldown();
+	void set_cooldown(float cd);
 };
 

632b76997a2ec81890ec1bd85bdddd586209fc59
Unknown <nolse16@student.aau.dk>
2017-11-02 10:43:08 +0100

    opencvworksrip

--- santa_game/santa_game/Player.h
+++ santa_game/santa_game/Player.h
@@ -1,6 +1,10 @@
 #pragma once
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include <opencv2/highgui/highgui.hpp> // import no include errors
+#include <opencv2/imgproc/imgproc.hpp> // import no include errors 
+#include <opencv2/core/core.hpp>       // import no include errors
+
+using namespace cv;
+using namespace std;
 
 class Player
 {

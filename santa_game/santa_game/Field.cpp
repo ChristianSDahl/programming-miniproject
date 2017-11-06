fac2db575de4191cae530828c5010bdcf2345336
Line Kjær Christensen <lkch16@student.aau.dk>
2017-11-06 10:30:09 +0100

    I still want to pull

--- santa_game/santa_game/Field.cpp
+++ santa_game/santa_game/Field.cpp
@@ -1,9 +1,25 @@
 #include "stdafx.h"
 #include "Field.h"
+#include <SFML\Graphics.hpp>
 
+using namespace std;
+using namespace cv;
+using namespace sf;
 
 Field::Field()
 {
+	RenderWindow renderWindow(sf::VideoMode(1280, 720), "My window");
+
+	sf::Texture texture;
+	if (texture.loadFromFile("maxresdefault.jpg") != true)
+
+	sf::Sprite sprite(texture);
+	while (true) {
+		renderWindow.clear();
+		sprite.setPosition(200, 200);
+		renderWindow.draw(sprite, sf::RenderStates::Default);
+		renderWindow.display();
+	}
 }
 
 

9c68fcf4c498605c8b9dd7d31012b1f375061fd0
madsgrumstrupjensen <madjen16@student.aau.dk>
2017-11-06 10:21:21 +0100

    serverigen

---
+++ cmake-build-debug/CMakeFiles/programming_miniproject.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Mads Grumstrup/programming-miniproject/Client/Client/Client.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/Client/Client/Client.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/Client/Client/stdafx.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/Client/Client/stdafx.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/ftp/Ftp.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/ftp/Ftp.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/opengl/OpenGL.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/opengl/OpenGL.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/pong/Pong.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/pong/Pong.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/shader/Shader.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/shader/Shader.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/sockets/Sockets.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sockets/Sockets.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/sockets/TCP.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sockets/TCP.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/sockets/UDP.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sockets/UDP.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/sound-capture/SoundCapture.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sound-capture/SoundCapture.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/sound/Sound.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sound/Sound.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/voip/Client.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/voip/Client.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/voip/Server.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/voip/Server.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/voip/VoIP.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/voip/VoIP.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/win32/Win32.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/win32/Win32.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/External Libraries/SFML/examples/window/Window.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/window/Window.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/ConsoleApplication3.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/ConsoleApplication3.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/Drops.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Drops.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/Elf.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Elf.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/Field.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Field.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/Player.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Player.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/Props.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Props.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/Santa.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Santa.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/santa_game.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/santa_game.cpp.obj"
+  "C:/Users/Mads Grumstrup/programming-miniproject/santa_game/santa_game/stdafx.cpp" "C:/Users/Mads Grumstrup/programming-miniproject/cmake-build-debug/CMakeFiles/programming_miniproject.dir/santa_game/santa_game/stdafx.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/programming_miniproject.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programming_miniproject.dir/Client/Client/Client.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/Client/Client/stdafx.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/ftp/Ftp.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/opengl/OpenGL.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/pong/Pong.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/shader/Shader.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sockets/Sockets.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sockets/TCP.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sockets/UDP.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sound/Sound.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/sound-capture/SoundCapture.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/voip/Client.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/voip/Server.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/voip/VoIP.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/win32/Win32.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/External_Libraries/SFML/examples/window/Window.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/ConsoleApplication3.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Drops.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Elf.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Field.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Player.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Props.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/Santa.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/santa_game.cpp.obj"
+  "CMakeFiles/programming_miniproject.dir/santa_game/santa_game/stdafx.cpp.obj"
+  "programming_miniproject.pdb"
+  "programming_miniproject.exe"
+  "programming_miniproject.exe.manifest"
+  "libprogramming_miniproject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/programming_miniproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

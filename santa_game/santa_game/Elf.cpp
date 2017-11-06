4bde8665f43ebc0dee5fbda8d3896ae58c44c6e4
Unknown <nolse16@student.aau.dk>
2017-11-06 10:24:22 +0100

    Elf + stuff

---
+++ santa_game/santa_game/Elf.cpp
@@ -0,0 +1,29 @@
+#include "Elf.h"
+
+
+
+Elf::Elf(float x)
+	: Player(x)
+{
+	//Sets y-value for the santa object
+	set_y(820);
+	//Sets x-value for the santa object
+	set_x(x);
+}
+
+
+Elf::~Elf()
+{
+}
+
+void Elf::action() {
+
+}
+
+float Elf::get_y() {
+	return y;
+}
+
+void Elf::set_y(float y) {
+	this->y = y;
+}

4aef8f422e674fc81d650d5ef9b8716806fa6833
Unknown <george.nedelev@gmail.com>
2017-11-07 11:46:24 +0100

    Comments

--- santa_game/santa_game/ServerG.cpp
+++ santa_game/santa_game/ServerG.cpp
@@ -14,30 +14,32 @@ class Server
 public:
 
 	int main() {
-	
 
 
-		sf::IpAddress a1 = sf::IpAddress::getLocalAddress();
-		sf::TcpSocket socket;
-		char connectionType, mode;
-		char buffer[2000];
 
-		std::size_t recieved;
-		std::string text = "connected to:";
+		sf::IpAddress a1 = sf::IpAddress::getLocalAddress(); //creating the server,  setting ip address on of it to your local computers ip
+		sf::TcpSocket socket; // creating a socket which will be needed to recieve data
 
-		std::cout << "Enter (s) for server , C for client:" << std::endl;
-		cin >> connectionType;
+		char connectionType, mode; // connectionType will decide which of the 2 users is a server ora  a client  ,,
 
-		if (connectionType == 's') {
 
-			sf::TcpListener listener;
-			listener.listen(3000);
-			listener.accept(socket);
-			text += "server";
-			mode = 's';
+		char buffer[2000];// creating a char buffer which will contain the data sent from the client to the server and back
 
-			socket.send(text.c_str(), text.length() + 1);
-			cin >> connectionType;
+		std::size_t recieved; // the size of the data recived 
+		std::string text = "connected to:"; //  this string represents the message that the cliend and the server would recieve 
+
+		std::cout << "Enter (s) for server , C for client:" << std::endl; // this message shows on the screen to take the users input 
+		cin >> connectionType; // listens for input from the user 
+
+		if (connectionType == 's') { // this if statement will execute if the user types s for a Servere
+
+			sf::TcpListener listener; // creating a listener 
+			listener.listen(3000); //  setting a port number of the listener
+			listener.accept(socket);// if the listener recives data it willa accept the socket
+			text += "server";  // this message extends the message that the clend and the server will recieve 
+			mode = 's'; // that variable is to change the modes of them whenever they send or recieve data ( not working yet)
+
+			socket.send(text.c_str(), text.length() + 1); // sendind the extended text to to the client 
 
 
 
@@ -45,22 +47,22 @@ public:
 
 		}
 
-		else if (connectionType == 'c')
+		else if (connectionType == 'c') // if the user pressed c for Client  
 		{
 
-			socket.connect(a1, 3000);
-			text += "client";
-			mode = 'r';
-			cin >> connectionType;
+			socket.connect(a1, 3000); //  client connecting to the socket a1 on a prot of 3000
+			text += "client"; // extending the message
+			mode = 'r';//  still working on it
+
 
 
 		}
 
 		socket.send(text.c_str(), text.length() + 1);
 
-		socket.receive(buffer, sizeof(buffer), recieved);
-	std::cout << buffer << std::endl;
-		std::getline(cin, text);
+		socket.receive(buffer, sizeof(buffer), recieved); // the socket saves in the variable buffer the size and the content of the message that we want to send
+	std:cout << buffer << std::endl;
+		std::getline(cin, text);// gets the typed text into a string 
 
 
 
@@ -79,13 +81,14 @@ public:
 			else if (mode == 'r') {
 				socket.receive(buffer, size(buffer), recieved);
 				if (recieved > 0) {
-				cout << "received" << buffer << endl;
+					std::cout << "received" << buffer << std::endl;
 					mode = 's';
 
 				}
 			}
-
-		}
+		}	
+	
+		
 		system("pause");
 		cin.ignore();
 		return 0;
